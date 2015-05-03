aa62b5fc12c72e86e75dcd0268be8d9eb338fab2
Renoulin Christian <chris-ren@netcourrier.com>
2015-05-03 10:05:54 +0200

    page d accueil : pas version

--- compil_Android_Cordova.bat
+++ compil_Android_Cordova.bat
@@ -13,17 +13,17 @@ SET JAVA_HOME=C:\dev\jdk1.7.0_01
 SET ANT_HOME=C:\dev\tools\apache-ant-1.9.4
 SET ANDROID_HOME=C:\dev\android-sdk\sdk
 SET ANDROID_SDK=C:\dev\android-sdk\sdk
-
+SET PATH=%PATH%;C:\dev\tools\sencha-cmd-5.0.2.270
 del /Q %PROJECT_DIR%\build\testing\MieuxTrierANantes\dev.apk
 
 sencha --info app build native
-cd %PROJECT_DIR%\cordova\platforms\android
-ant release -Dkey.store=%KEY_STORE% -Dkey.alias=%KEY_ALIAS% -Dkey.store.password=%KEY_STORE_PASSWORD% -Dkey.alias.password=%KEY_ALIAS_PASSWORD%
+rem cd %PROJECT_DIR%\cordova\platforms\android
+rem ant release -Dkey.store=%KEY_STORE% -Dkey.alias=%KEY_ALIAS% -Dkey.store.password=%KEY_STORE_PASSWORD% -Dkey.alias.password=%KEY_ALIAS_PASSWORD%
 
-copy %PROJECT_DIR%\cordova\platforms\android\bin\CordovaApp-release.apk %PROJECT_DIR%\build\testing\MieuxTrierANantes\dev.apk
+rem copy %PROJECT_DIR%\cordova\platforms\android\bin\CordovaApp-release.apk %PROJECT_DIR%\build\testing\MieuxTrierANantes\dev.apk
 
 
 rem tempo
-cd C:\dev\github-repositories\MieuxTrierANantes
+rem cd C:\dev\github-repositories\MieuxTrierANantes
 
 pause
