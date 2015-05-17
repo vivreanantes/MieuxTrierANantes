428c48f210dc7e32ca8685dfc70c3290296ca456
Renoulin Christian <chris-ren@netcourrier.com>
2015-05-17 22:59:39 +0200

    quiz

--- resources/datas/LabelsDatas.js
+++ resources/datas/LabelsDatas.js
@@ -1,10 +1,10 @@
 var _labelsDatas = {
 	"about_titre" : {
-	"fr" : "Mieux Trier à Nantes v.1.3"
+	"fr" : "Mieux Trier à Nantes, 1.13"
 	},
 	"about" : {
-		"fr" : "Logiciel libre (code, photos, données).<br/>"
-				+ "Auteurs : Christian Renoulin, Romain Debernardi<br/>"
+		"fr" : "Logiciel libre (code, photos, données), par Christian Renoulin et Romain Debernardi<br/>"
+				+ "Voir <a href='http://mieuxtrieranantes.fr/index.html?fiche=fiche_mieuxtrierannantes'>Fiche 'Mieux trier à Nantes'"
 				+ "<br/>"
 				+ "<a href='https://mieuxtrieranantes.wordpress.com'>blog</a> "
 				+ " <A HREF='https://www.facebook.com/pages/Mieux-trier-à-Nantes/266725983531595' TARGET=_blank title='Facebook'><IMG SRC='resources/icons/facebook.png' HEIGHT='18px' style='vertical-align:middle;'/></A> "
@@ -13,7 +13,6 @@ var _labelsDatas = {
 				+ "<IMG SRC='resources/icons/twitter.png' HEIGHT='18px' style='vertical-align:middle;'/><font size='2'>@MieuxTrierANant</font><br/>"
 				+ "Contacts : <i><a href='mailto:mieuxtrieranantes@gmail.com'><font size='2'>mieuxtrieranantes@gmail.com</font></a></i><br/>"
 				+ "<br/>"
-				+ "Nos amis : <IMG SRC='resources/images/images_non_libres/logo_les_boites_vertes_rogne_mini.png' HEIGHT='22px' style='vertical-align:middle;'/>&nbsp;&nbsp;<A HREF='http://lesboitesvertes.fr/' TARGET=_blank title='Association les boîtes vertes (Baludik, PlusDeVers)'>les boîtes vertes</A><br/>"
-				+ "Voir <a href='http://mieuxtrieranantes.fr/index.html?fiche=fiche_mieuxtrierannantes'>Fiche Mieux trier à Nantes"
+				+ "Nos amis : <IMG SRC='resources/images/images_non_libres/logo_les_boites_vertes_rogne_mini.png' HEIGHT='22px' style='vertical-align:middle;'/>&nbsp;&nbsp;<A HREF='http://lesboitesvertes.fr/' TARGET=_blank title='Association les boîtes vertes (Baludik, PlusDeVers)'>les boîtes vertes</A>"
 	}
 };
