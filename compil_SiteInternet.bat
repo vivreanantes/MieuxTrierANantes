bbd812a5e0273a6716611c42324d84f5dc573180
Renoulin Christian <chris-ren@netcourrier.com>
2015-08-06 20:51:19 +0200

    modif

--- compil_SiteInternet.bat
+++ compil_SiteInternet.bat
@@ -3,7 +3,7 @@ SET PROJECT_DIR=C:\dev\github-repositories\MieuxTrierANantes
 del /Q /S %PROJECT_DIR%\build\testing\MieuxTrierANantes\archive
 
 cd C:\dev\github-repositories\MieuxTrierANantes
-C:\dev\tools\sencha-cmd-5.0.2.270\sencha app build testing
+C:\dev\tools\sencha-cmd-5.1.3.61\sencha app build testing
 
 xcopy %PROJECT_DIR%\dist %PROJECT_DIR%\build\testing\MieuxTrierANantes\ /E /Y
 xcopy %PROJECT_DIR%\favicon.ico %PROJECT_DIR%\build\testing\MieuxTrierANantes\ /Y
